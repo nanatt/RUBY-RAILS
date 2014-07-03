9eb3e112cd5f3ec0a9cfda30a451183bfd94d7ce
Nathália <nathaliasoouza@gmail.com>
2014-07-03 18:11:59 -0300

    Associations 1-N and N-N(2) in the old models and files

--- RubyonRails/blog/app/models/user.rb
+++ RubyonRails/blog/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-	has_one :profile
+	has_one :profile, :dependent => :destroy
+	has_many :articles, -> { order('published_at DESC, title ASC')}, :dependent => :destroy
+	has_many :replies, :through => :articles, :source => :comments
 end
