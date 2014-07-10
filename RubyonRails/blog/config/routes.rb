b102b82710f7d99d29057eb44fa309000f919401
Nathália <nathaliasoouza@gmail.com>
2014-07-10 18:37:37 -0300

    Adding Comments and Users to routes

--- RubyonRails/blog/config/routes.rb
+++ RubyonRails/blog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root :to => "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  resources :users
 end
  # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
