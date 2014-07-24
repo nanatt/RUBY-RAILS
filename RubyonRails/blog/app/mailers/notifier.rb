a60ed0b29ff00ae833da4a17ee12a2086bfcf43f
Nathália <nathaliasoouza@gmail.com>
2014-07-24 18:58:39 -0300

    Everytime that a mail action is created, it call directly the mailer class. Argument to set a meaningful title and use the name and email parameters colletcted from the form(in a method)

--- RubyonRails/blog/app/mailers/notifier.rb
+++ RubyonRails/blog/app/mailers/notifier.rb
@@ -6,9 +6,9 @@ class Notifier < ActionMailer::Base
   #
   #   en.notifier.email_friend.subject
   #
-  def email_friend
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def email_friend(article, sender_name, receiver_email)
+    @article = article
+    @sender_name = sender_name
+    mail to: receiver_email, subject: "Interesting Article"
   end
 end
