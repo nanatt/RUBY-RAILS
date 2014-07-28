9c1dbf62ad496195634e8cde25835b16285a9699
Nathália <nathaliasoouza@gmail.com>
2014-07-28 18:30:02 -0300

    Integration Testing: it can span multiple controllers and actions with full session support. Login and logout examples

---
+++ RubyonRails/blog/test/integration/user_stories_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UserStoriesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should login user and redirect" do
+  	get login_path
+
+  	assert_response :success
+  	assert_template 'new'
+
+  	post session_path, :email => 'lauren@example.com', :password => 'secret'
+
+  	assert_response :redirect
+  	assert_redirected_to root_path
+
+  	follow_redirect!
+
+  	assert_response :success
+  	assert_template 'index'
+  	assert session[:user_id]
+  end
+  test "should logout user and redirect" do
+    get logout_path
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_nil session[:user]
+    follow_redirect!
+    assert_template 'index'
+  end
+
+end
