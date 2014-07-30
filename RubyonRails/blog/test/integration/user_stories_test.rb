aeed50e555d31d2459cd5748bdb0a93303686504
Nathália <nathaliasoouza@gmail.com>
2014-07-30 17:39:52 -0300

    Updated Integration by private methods test

--- RubyonRails/blog/test/integration/user_stories_test.rb
+++ RubyonRails/blog/test/integration/user_stories_test.rb
@@ -4,46 +4,48 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "should login create article and redirect" do
-    #Login
-  	get login_path
-
-  	assert_response :success
-  	assert_template 'new'
-
-  	post session_path, :email => 'lauren@example.com', :password => 'secret'
-
-  	assert_response :redirect
-  	assert_redirected_to root_path
-
-  	follow_redirect!
-
-  	assert_response :success
-  	assert_template 'index'
-  	assert session[:user_id]
-  #Create New Article
-  get new_article_path
-
-  assert_response :success
-  assert_template 'new'
-
-  post articles_path, :article => {title: 'Integration Tests', body: "Let's do this"}
-  assert assigns(:article).valid?
-  assert_response :redirect
-  assert_redirected_to article_path(assigns(:article))
-
-  follow_redirect!
-
-  assert_response :success
-  assert_template 'show'
-  
-  #Logout
-    get logout_path
-    assert_response :redirect
-    assert_redirected_to root_path
-    assert_nil session[:user]
-    follow_redirect!
-    assert_template 'index'
+  test "creating an article" do
+    lauren = registered_user
+    lauren.logs_in 'lauren@example.com', 'secret'
+    lauren.creates_article title: 'Integration tests', body: "Lets do thissss"
+    lauren.logs_out
   end
-
+  
+  private
+  def registered_user
+    open_session do |user|
+      def user.logs_in(email, password)
+        get login_path
+        assert_response :success
+        assert_template 'new'
+        post session_path, :email => email, :password => password
+        assert_response :redirect
+        assert_redirected_to root_path
+        follow_redirect!
+        assert_response :success
+        assert_template 'index'
+        assert session[:user_id]
+      end
+      def user.logs_out
+        get logout_path
+        assert_response :redirect
+        assert_redirected_to root_path
+        assert_nil session[:user]
+        follow_redirect!
+        assert_template 'index'
+      end
+      def user.creates_article(article_hash)
+        get new_article_path
+        assert_response :success
+        assert_template 'new'
+        post articles_path, :article => article_hash
+        assert assigns(:article).valid?
+        assert_response :redirect
+        assert_redirected_to article_path(assigns(:article))
+        follow_redirect!
+        assert_response :success
+        assert_template 'show'
+      end
+    end
+  end
 end
