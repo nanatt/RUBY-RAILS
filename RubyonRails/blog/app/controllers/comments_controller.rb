84720d6ef502a98341e33ded54b5c2cf5ff50a77
Nathália <nathaliasoouza@gmail.com>
2014-08-04 15:14:51 -0300

    Updating controller to accept only logged_in users comments

--- RubyonRails/blog/app/controllers/comments_controller.rb
+++ RubyonRails/blog/app/controllers/comments_controller.rb
@@ -1,16 +1,14 @@
 class CommentsController < ApplicationController
 	before_filter :load_article, :except => :destroy
-	before_filter :authenticate, :only => :destroy
+	before_filter :authenticate
 
 	def create
 		@comment = @article.comments.new(comment_params)
-		if @comment.save
-			respond_to do |format|
+		respond_to do |format|
+			if @comment.save
 				format.html {redirect_to @article, notice: 'Thanks for your comment'}
 				format.js
-			end
-		else
-			respond_to do |format|
+			else
 				format.html{redirect_to @article, alert: "Unable to add comment"}
 				format.js {render 'fail_create.js.erb'}
 			end
@@ -18,12 +16,15 @@ class CommentsController < ApplicationController
 	end
 
 	def destroy
-		@article = current_user.articles.find(params[:article_id])
-		@comment = @article.comments.find(params[:id])
-		if @comment.destroy
-			respond_to do |format|
+		#@article = current_user.articles.find(params[:article_id])
+		@comment = current_user.replies.find(params[:id])
+		respond_to do |format|
+			if @comment.destroy
 				format.html {redirect_to @article, notice: 'Comment Deleted'}
 				format.js
+			else
+				format.html
+				format.json
 			end
 		end
 	end
