6d32a794ff25e9183acff2b91d807abe037d647a
Nathália <nathaliasoouza@gmail.com>
2014-08-07 15:16:25 -0300

    Making validations

--- RubyonRails/depot/app/models/product.rb
+++ RubyonRails/depot/app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+	validates_presence_of :title, :description, :image_url
+	validates_numericality_of :price, greater_than: 0
+
+	validates_uniqueness_of :title
+	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png|jpeg)\Z}i, 
+	message: 'must be a URL for GIF, JPG, PNG, JPEG image.'}
 end
