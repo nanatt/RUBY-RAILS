b72b851b6a373ee7c781c58596af0469f69e487c
Nathália <nathaliasoouza@gmail.com>
2014-08-07 19:07:07 -0300

    Product edition on tests, models, routes and config

--- RubyonRails/depot/app/models/product.rb
+++ RubyonRails/depot/app/models/product.rb
@@ -5,4 +5,21 @@ class Product < ActiveRecord::Base
 	validates_uniqueness_of :title
 	validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png|jpeg)\Z}i, 
 	message: 'must be a URL for GIF, JPG, PNG, JPEG image.'}
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	def self.latest 
+		Product.order(:updated_at).last
+	end
+
+	private
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
 end
