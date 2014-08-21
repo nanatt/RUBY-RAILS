69cfa807fe689b43e3f57ab0e90bc4d5f880dda1
Nathália <nathaliasoouza@gmail.com>
2014-08-21 18:45:59 -0300

    Mailer modifications; Sending Confirmation Emails

--- RubyonRails/depot/app/mailers/order_notifier.rb
+++ RubyonRails/depot/app/mailers/order_notifier.rb
@@ -1,25 +1,32 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "depot@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
+  #def received
+  #  @greeting = "Hi"
 
-    mail to: "to@example.org"
-  end
+  #  mail to: "to@example.org"
+  #end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier.shipped.subject
   #
-  def shipped
-    @greeting = "Hi"
+  def shipped(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+  #added code to copy the parameter passed into an instance variable and update the call 
+  # to mail() specifying where to send the email and what subject line to use
+  def received(order)
+    @order = order
+    
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 end
