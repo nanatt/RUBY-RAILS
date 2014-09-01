f52d0f014793b798194e2165825bc177b37f02f6
Nathália <nathaliasoouza@gmail.com>
2014-09-01 18:54:49 -0300

    Internationalization in English and Spanish

--- RubyonRails/depot/config/routes.rb
+++ RubyonRails/depot/config/routes.rb
@@ -19,7 +19,12 @@ Rails.application.routes.draw do
     get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
