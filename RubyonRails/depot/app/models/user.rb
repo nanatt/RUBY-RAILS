1ba4ca9ecd2140016fb69f6b8e8e6c56a2e0d97a
Nathália <nathaliasoouza@gmail.com>
2014-09-01 19:00:42 -0300

    I18n on application controller and exception on users controller; method that last user cant be deleted; updated email development;

--- RubyonRails/depot/app/models/user.rb
+++ RubyonRails/depot/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
 	validates_presence_of :name, :email, uniqueness: true
   	has_secure_password
+  	after_destroy :ensure_an_admin_remains
+
+  	private
+  	def ensure_an_admin_remains
+  		if User.count.zero?
+  			raise "Can't delete last user"
+  		end
+  	end
 end
