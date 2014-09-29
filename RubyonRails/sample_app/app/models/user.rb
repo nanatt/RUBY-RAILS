d1e85aeca2b8b4ebf3a4979c29be9c4dd803ce81
Nathália <nathaliasoouza@gmail.com>
2014-09-29 18:47:01 -0300

    User modifications

--- RubyonRails/sample_app/app/models/user.rb
+++ RubyonRails/sample_app/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	validates_presence_of :name, length: { maximum: 50 }
+
+	before_save { self.email = email.downcase }
+	validates_presence_of :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}, uniqueness: true
+
+	has_secure_password
 end
