c19c43eac7340a38df20fdf5e9c5ec045caa4775
Nathália <nathaliasoouza@gmail.com>
2014-10-13 17:41:20 -0300

    Updated css and errors

--- RubyonRails/sample_app/app/controllers/users_controller.rb
+++ RubyonRails/sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -11,6 +15,8 @@ class UsersController < ApplicationController
   	def create
   		@user = User.new(user_params)
   		if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
   		else
   			render 'new'
   		end
